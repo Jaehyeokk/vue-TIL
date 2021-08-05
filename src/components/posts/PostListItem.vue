22c2f8f4e5b3e1bfffb36ed774cbec30a9af17f3
Jaehyeokk <lionjk92@naver.com>
2021-08-05 21:37:10 +0900

    API - Get posts

---
+++ src/components/posts/PostListItem.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<p>{{ post.title }}</p>
+		<p>{{ post.contents }}</p>
+		<p>{{ post.createdAt }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		post: {
+			type: Object,
+			require: true,
+		},
+	},
+};
+</script>
+
+<style></style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,12 +1,50 @@
 <template>
-	<div>mainpage</div>
+	<div class="main-page">
+		<div class="container">
+			<LoadingSpinner v-if="loading"></LoadingSpinner>
+			<template v-else>
+				<PostListItem
+					v-for="post in posts"
+					:key="post._id"
+					:post="post"
+				></PostListItem>
+			</template>
+		</div>
+	</div>
 </template>
 
 <script>
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+import PostListItem from '@/components/posts/PostListItem.vue';
+import { fetchPosts } from '@/apis/index.js';
 import { mapState } from 'vuex';
 export default {
+	components: {
+		PostListItem,
+		LoadingSpinner,
+	},
+	data() {
+		return {
+			posts: [],
+			loading: false,
+		};
+	},
+	created() {
+		this.loading = true;
+		this.loadPosts().then(() => (this.loading = false));
+	},
 	computed: {
 		...mapState(['loginUser']),
+	},
+	methods: {
+		async loadPosts() {
+			try {
+				const { data } = await fetchPosts();
+				this.posts = data.posts;
+			} catch (error) {
+				console.log(error.message);
+			}
+		},
 	},
 };
 </script>
