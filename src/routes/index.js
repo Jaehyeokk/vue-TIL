160a7b3d8650b143f8affcd9b85b871650e49b9a
Jaehyeokk <lionjk92@naver.com>
2021-08-10 21:11:00 +0900

    Apply router navigation guard

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index.js';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
 	mode: 'history',
 	routes: [
 		{
@@ -21,18 +22,41 @@ export default new VueRouter({
 		{
 			path: '/main',
 			component: () => import('@/views/MainPage.vue'),
+			meta: {
+				auth: true,
+			},
 		},
 		{
 			path: '/add',
 			component: () => import('@/views/PostAddPage.vue'),
+			meta: {
+				auth: true,
+			},
 		},
 		{
 			path: '/post/:id',
 			component: () => import('@/views/PostEditPage.vue'),
+			meta: {
+				auth: true,
+			},
 		},
 		{
 			path: '*',
 			component: () => import('@/views/NotFoundPage.vue'),
+			meta: {
+				auth: true,
+			},
 		},
 	],
 });
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.auth && !store.getters.isLogin) {
+		next('/login');
+		console.log('인증이 필요합니다.');
+		return;
+	}
+	next();
+});
+
+export default router;
