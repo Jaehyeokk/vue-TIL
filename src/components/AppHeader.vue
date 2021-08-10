3bfc5430fa89164eaf82c8dd7b92b3d38d7d5597
Jaehyeokk <lionjk92@naver.com>
2021-08-10 21:12:39 +0900

    Link change according to login status

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,8 @@
 <template>
 	<header>
+		<h1 class="logo">
+			<router-link :to="logo_link">로고</router-link>
+		</h1>
 		<template v-if="isLogin">
 			{{ `${this.$store.state.username}님 환영합니다.` }}
 			<a href="javascript:;" @click="logout">로그아웃</a>
@@ -17,6 +20,13 @@ import { mapGetters } from 'vuex';
 export default {
 	computed: {
 		...mapGetters(['isLogin']),
+		logo_link() {
+			if (this.isLogin) {
+				return '/main';
+			} else {
+				return '/login';
+			}
+		},
 	},
 	methods: {
 		logout() {
